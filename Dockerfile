3b4daa889e5e6bc26e1f0e66a335fc0a109ac81e
lexandro <lexandro2000@gmail.com>
2015-04-28 17:58:19 +0200

    adding new maven version

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM lexandro/java8-base
 
 MAINTAINER Robert Stern <lexandro2000@<you know that big search company>.com
 
-RUN wget -qO- http://mirror.dkd.de/apache/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz|tar zxv
+ADD apache-maven-3.3.1 /apache-maven-3.3.1
 
 ENV M2_HOME apache-maven-3.3.1
+
 ENV PATH $M2_HOME/bin:$PATH
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
-if [ ! -f apache-maven-3.2.5-bin.tar.gz ]; then
-   wget -qO- http://www.eu.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz|tar xvf
+if [ ! -f apache-maven-3.3.1/. ]; then
+   wget -qO- http://www.eu.apache.org/dist/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz|tar zxv
 fi
 
 docker build --force-rm=true -t lexandro/java8-maven3 .
